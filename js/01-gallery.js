f4d6bde368f20b160b592f403ff4ec263190fdcd
AlexKosha <ovelikoshich@gmail.com>
2023-09-08 23:36:03 +0200

    save1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,55 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector(".gallery");
+
+const galleryImages = createMarkupGallery(galleryItems);
+
+let instance = null;
+
+gallery.insertAdjacentHTML("beforeend", galleryImages);
+
+gallery.addEventListener("click", onOpenModalClick);
+
+gallery.addEventListener("keydown", onCloseModalClick);
+
+function createMarkupGallery(images) {
+  return images
+    .map(
+      ({ preview, description, original }) => `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`
+    )
+    .join("");
+}
+
+function onOpenModalClick(evt) {
+  evt.preventDefault();
+  if (evt.target.classList.contains("gallery__image")) {
+    const imageSrc = evt.target.getAttribute("data-source");
+    console.log(imageSrc);
+    const description = evt.target.getAttribute("alt");
+
+    instance = basicLightbox.create(
+      `<div class = "modal"></div><img src="${imageSrc}" alt="${description} "/></div>`
+    );
+
+    instance.show();
+  }
+}
+
+function onCloseModalClick(evt) {
+  evt.preventDefault();
+  if (evt.code === "Escape" && basicLightbox.visible()) {
+    instance.close();
+  }
+}
